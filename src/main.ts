863195c73ab23d1a5897afa532f85411930fc8b8
MOLO-7 <am8736419@gmail.com>
2022-07-14 14:19:47 +0200

    handle page newly created blocks

--- src/main.ts
+++ src/main.ts
@@ -38,4 +38,30 @@ function main() {
   document
     .querySelectorAll(autoDirElementsSelectors)
     .forEach((ele) => ele.setAttribute("dir", "auto"));
+
+  // handle newly created blocks
+  const mutationObserver = new MutationObserver((records) => {
+    records.forEach((record) => {
+      if (record.addedNodes) {
+        record.addedNodes.forEach((node) => {
+          if (node.nodeType !== Node.ELEMENT_NODE) return;
+          const block = node as Element;
+
+          // auto dir check
+          if (
+            autoDirElementsSelectors
+              .split(",")
+              .some((selector) => block.matches(selector))
+          ) {
+            block.setAttribute("dir", "auto");
+          }
+        });
+      }
+    });
+  });
+
+  mutationObserver.observe(document.querySelector(".notion-page-content")!, {
+    childList: true,
+    subtree: true,
+  });
 }
